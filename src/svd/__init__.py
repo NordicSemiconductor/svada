70d868be5ce96c60fdf40026b0eb68ece72a93ba
Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>
2024-11-20 13:03:38 +0100

    svd: add public APIs to __all__
    __all__ is used by some linters to check if an API is public.
    
    Signed-off-by: Jonathan Nilsen <jonathan.nilsen@nordicsemi.no>

--- src/svd/__init__.py
+++ src/svd/__init__.py
@@ -50,4 +50,51 @@ import importlib.metadata
 
 __version__ = importlib.metadata.version("svada")
 
+__all__ = [
+    # from bindings
+    "Access",
+    "ReadAction",
+    "Endian",
+    "SauAccess",
+    "AddressBlockUsage",
+    "Protection",
+    "EnumUsage",
+    "WriteAction",
+    "DataType",
+    "CpuName",
+    "Cpu",
+    "AddressBlock",
+    "SauRegion",
+    # from errors
+    "SvdError",
+    "SvdParseError",
+    "SvdDefinitionError",
+    "SvdMemoryError",
+    "SvdPathError",
+    "SvdIndexError",
+    "SvdKeyError",
+    # from parsing
+    "parse",
+    "Options",
+    # from path
+    "FEPath",
+    "EPath",
+    # from device
+    "Array",
+    "Field",
+    "FlatRegister",
+    "FlatRegisterUnion",
+    "FlatRegisterType",
+    "FlatStruct",
+    "FlatField",
+    "Device",
+    "Peripheral",
+    "Register",
+    "RegisterUnion",
+    "RegisterType",
+    "Struct",
+    # other
+    "__version__",
+]
+
 del importlib.metadata
